efd8b06515dd342d1f0129839b42567d303b532b
toanvo <toantvo@gmail.com>
2019-11-01 06:19:02 +0700

    Initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await TestHttpClientFactory();
+            Console.ReadLine();
+        }
+
+        public static async Task TestHttpClientFactory()
+        {
+            var serviceProvider = new ServiceCollection().AddHttpClient().BuildServiceProvider();
+            var httpClientFactory = serviceProvider.GetService<IHttpClientFactory>();
+
+            var client = httpClientFactory.CreateClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, "http://www.google.com");
+            var response = await client.SendAsync(request);
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(content);
+        }
+    }
+}
